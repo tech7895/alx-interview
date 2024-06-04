cf1382d0365b2ff5ef0da16ef4f7291a5d31c066
tech7895 <coper5540@gmail.com>
2024-06-04 02:47:28 -0700

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+"""
+This script is a method that determines if all the boxes can be opened
+
+"""
+
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+
+    queue = [0]
+    while queue:
+        current = queue.pop(0)
+        for key in boxes[current]:
+            if key < n and not opened[key]:
+                opened[key] = True
+
+                queue.append(key)
+
+    return all(opened)
